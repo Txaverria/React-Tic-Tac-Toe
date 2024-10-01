6f5da8413c384e38d67b317238dec31b0ce9a606
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-01 06:58:27 -0600

    Update Player to dynamically change info

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function manageEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required placeholder={name}></input>;
+    btnCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={manageEditing}>{btnCaption}</button>
     </li>
   );
 }
