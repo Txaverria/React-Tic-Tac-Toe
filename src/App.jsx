36451a67ac9b70efdc0c7ba92a770752212e1675
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-01 13:54:19 -0600

    Add GameBoard component with working X buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
@@ -7,8 +8,8 @@ function App() {
         <ol id="players">
           <Player initialName={"Player 1"} symbol={"X"} />
           <Player initialName={"Player 2"} symbol={"O"} />
-
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
