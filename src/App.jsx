7c7cdc013c241d04bdf6df8377b3f30bcf3fe843
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-10-03 14:52:22 -0600

    Add GameOver component to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./GameOver";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -45,7 +47,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner == firstSquareSymbol;
+      winner = firstSquareSymbol;
     }
   }
 
@@ -69,7 +71,7 @@ function App() {
           <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === "X"} />
           <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === "O"} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {winner && <GameOver winner={winner}></GameOver>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}></Log>
